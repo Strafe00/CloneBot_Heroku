aa79cb1ce81a633e91a225f65b1336a2d6b9a654
Strafe00 <101890795+Strafe00@users.noreply.github.com>
2022-04-15 17:18:11 +0300

    Update process.py

--- telegram_gcloner/utils/process.py
+++ telegram_gcloner/utils/process.py
@@ -14,20 +14,20 @@ logger = logging.getLogger(__name__)
 
 def leave_chat_from_message(message, context):
     context.bot.send_message(chat_id=message.chat_id,
-                             text='Hey, thank you for adding ⚡️ CloneBot to this group. ' + config.AS_STRING.format(context.bot.username),
+                             text='Clonebot\'u eklediğiniz için teşekkürler. ' + config.AS_STRING.format(context.bot.username),
                              parse_mode=ParseMode.HTML)
-    context.bot.send_message(chat_id=message.chat_id, text='\n\nUnfortunately I am not allowed to be here 😔 \nIf you want me in this group, ask my owner to authorize me 😉.')
+    context.bot.send_message(chat_id=message.chat_id, text='\n\nBu grupta izinli değilim. Lütfen yöneticimle görüşün.')
     if message.from_user:
         mention_html_from_user = mention_html(message.from_user.id,
                                               message.from_user.full_name.full_name)
-        text = '🔙 Left unauthorized group : \n │ Name : {} ({}). \n │ Added by : {} {}. \n │ Message : {}'.format(
+        text = '🔙 Kalan Yetkilendirilmemiş Gruplar : \n │ İsim : {} ({}). \n │ Bu kişi tarafından eklendi : {} {}. \n │ Mesaj : {}'.format(
             html.escape(message.chat.title),
             message.chat_id,
             mention_html_from_user,
             message.from_user.id,
             message.text)
     else:
-        text = '🔙 Left unauthorized group : \n │ Name : {} ({}). \n │ Message : {}'.format(
+        text = '🔙 Kalan Yetkilendirilmemiş Gruplar : \n │ İsim : {} ({}). \n │ Mesaj : {}'.format(
             html.escape(message.chat.title),
             message.chat_id,
             message.text)
